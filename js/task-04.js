58703feb5c8140a5dd1df8c3400eaa858aed34df
Liubomyr <ljubik@ukr.net>
2020-10-24 13:09:49 +0300

    Task №04 complete

--- js/task-04.js
+++ js/task-04.js
@@ -1,43 +1,59 @@
-// Завдання 4
-// Задача 4-4
-// this в методах объекта
-// Расставь отсутствующие this в методах объекта account.
+// // Завдання 4
+// Задача 5-4
+// переиспользование методов класса
+// Напиши класс StringBuilder. На вход он получает один параметр - строку, 
+// которую записывает в свойство _value.
 
-// В комментариях показаны операции с объектом и ожидаемые результаты.
+// Добавь классу следующий функционал:
+
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает параметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает параметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает параметр str (строку) и добавляет ее в начало и в конец _value
+// Метод pad должен использовать методы append и prepend
+
 
 // Здано:
-const account = {
-	owner: 'Mango',
-	balance: 24000,
-	discount: 0.1,
-	orders: ['order-1', 'order-2', 'order-3'],
-	changeDiscount(value) {
-	  this.discount = value; // Write code in this line
-	},
-	showOrders() {
-	  return this.orders; // Write code in this line
-	},
-	addOrder(cost, order) {
-	  this.balance -= cost; // Write code in this line
-	  this.orders.push(order); // Write code in this line
-	},
-  };
-  const copyAccount =  Object.assign({},account);
-  copyAccount.orders = [...account.orders];
-  // копируем для автотестов ссылочные типы
-  
-  /*
-  account.changeDiscount(0.15);
-  //console.log(account.discount); // 0.15
-  
-  //console.log(account.showOrders()); 
-  // ['order-1', 'order-2', 'order-3']
-  
-  account.addOrder(5000, 'order-4');
-  //console.log(account.balance); // 19000
-  
-  //console.log(account.showOrders());
-  // ['order-1', 'order-2', 'order-3', 'order-4']
-  */
+ // Write code under this line
+ class StringBuilder{
+  constructor(string){
+    this._value = string
+  }
+   get value(){
+   return this._value
+   }
+   
+append(str){
+  this._value = this._value + str
+ return this._value
+  }
+   
+   prepend(str){
+    this._value = str + this._value 
+     return this._value
+   }
+   
+   pad(str){
+     //this.append()
+     this._value = str + this._value + str 
+     //this.prepend() 
+   return this._value
+   }
+   
+ }
 
-  //Просто дописать  this в строчках,которые указаны в задании (Write code in this line)
+
+ console.log(typeof StringBuilder);
+// 'function'
+
+
+ const builder = new StringBuilder('.');
+
+ builder.append('^');
+console.log(builder.value); // '.^'
+
+ builder.prepend('^');
+ console.log(builder.value); // '^.^'
+
+ builder.pad('=');
+ console.log(builder.value); // '=^.^='
